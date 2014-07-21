0a1364cbff306c69e1ac2bfe110acf1d033f6999
余泽江 <yzj1995@vip.qq.com>
2014-07-21 17:33:10 +0800

    实现开始/结束按钮切换及结束弹窗提示

--- app/scripts/controllers/activity_detail_controller.js
+++ app/scripts/controllers/activity_detail_controller.js
@@ -3,9 +3,12 @@
 angular.module('partyBidApp')
   .controller('ActivityDetailController', function ($scope, $location, $routeParams) {
 
+  	$scope.status_to_watch;
+
   	$scope.initiate_data = function () {
   		$scope.activity_name = $routeParams.name;
   		$scope.start_or_stop = Activity.is_running($scope.activity_name)? "结束": "开始";
+  		$scope.status_to_watch = $scope.start_or_stop;
   	};
 
   	$scope.initiate_data();
@@ -16,15 +19,21 @@ angular.module('partyBidApp')
 
     $scope.start_activity = function () {
 		if ($scope.start_or_stop == "开始") {
-			//$scope.start_or_stop ＝ "结束";
+			$scope.status_to_watch = "结束";
 	    	Activity.start_activity($scope.activity_name);
 		}
     	else {
     		if (window.confirm("确认要结束本次报名吗？！")) {
-    			//$scope.start_or_stop = "开始";
+    			$scope.status_to_watch = "开始";
     			Activity.stop_activity($scope.activity_name);
     		}
     	}
     }
+
+    function check_if_alter(newValue, oldValue, scope) {
+        $scope.start_or_stop = $scope.status_to_watch;
+    };
+
+    $scope.$watch('status_to_watch', check_if_alter, true);
 
   });
