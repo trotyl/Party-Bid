048fb07f0bf7c0f1089114e205f8628c28aa68b7
余泽江 <yzj1995@vip.qq.com>
2014-07-23 18:08:31 +0800

    实现用jade生成html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,3 +27,10 @@ angular
         redirectTo: '/'
       });
   });
+
+var jade = require('jade');
+console.log(jade);
+// Compile a function
+var fn = jade.compile('string of jade', options);
+console.log(fn);
+fn(locals);
--- app/scripts/pre.js
+++ app/scripts/pre.js
@@ -1,5 +1,6 @@
 var jade = require('jade');
-
+console.log(jade);
 // Compile a function
 var fn = jade.compile('string of jade', options);
+console.log(fn);
 fn(locals);
