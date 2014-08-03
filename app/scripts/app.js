f3eae4517c7efb932bf1f0e1d6a5e4adb858d812
余泽江 <yzj1995@vip.qq.com>
2014-08-03 16:00:07 +0800

    实现路由与app.js分离

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular
+var partyBidApp = angular
   .module('partyBidApp', [
     'ngAnimate',
     'ngCookies',
@@ -8,42 +8,7 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/activity_list.html',
-        controller: 'ActivityListController'
-      })
-      .when('/create', {
-        templateUrl: 'views/create_activity.html',
-        controller: 'CreateActivityController'
-      })
-      .when('/detail/:name', {
-        templateUrl: 'views/activity_detail.html',
-        controller: 'ActivityDetailController'
-      })
-      .when('/detail/:name/bid', {
-        templateUrl: 'views/bid_list.html',
-        controller: 'BidListController'
-      })
-      .when('/detail/:name/bid/:number', {
-        templateUrl: 'views/bid_detail.html',
-        controller: 'BidDetailController'
-      })
-      .when('/detail/:name/bid/:number/result', {
-        templateUrl: 'views/bid_result.html',
-        controller: 'BidResultController'
-      })
-      .when('/detail/:name/bid/:number/stats', {
-        templateUrl: 'views/bid_stats.html',
-        controller: 'BidStatsController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
-
+  ]);
 
 var native_access;
 $(document).ready(function () {
