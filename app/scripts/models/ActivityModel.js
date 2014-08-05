051c43c2e7a73c637fa090e483b5581f6494ccc3
余泽江 <yzj1995@vip.qq.com>
2014-08-05 11:42:03 +0800

    调整Activity中函数顺序

--- app/scripts/models/ActivityModel.js
+++ app/scripts/models/ActivityModel.js
@@ -6,42 +6,12 @@ function Activity(name, createdAt, register, bid, count) {
     this.count = count;
 }
 
-Activity.get_all_items = function () {
-    return JSON.parse(localStorage.getItem("activity_list")) || [];
-};
+//实例方法
 
 Activity.prototype.save = function () {
     var activity_list = Activity.get_all_items();
-    var activity_found = _(activity_list).findWhere({name: this.name})
-    activity_found? activity_found = this: activity_list.push(this);
+    activity_list.push(this);
     localStorage.setItem("activity_list", JSON.stringify(activity_list));
-};
-
-Activity.save_all = function(activity_list) {
-    localStorage.setItem("activity_list", JSON.stringify(activity_list));
-};
-
-Activity.update_current_activity = function (activity_to_update) {
-    localStorage.setItem("current_activity", JSON.stringify(activity_to_update));
-};
-
-Activity.get_current_item = function () {
-    return JSON.parse(localStorage.getItem("current_activity")) || new Activity("");
-};
-
-Activity.find_by_name = function (name_to_find) {
-    var activity_list = Activity.get_all_items();
-    var object_found = _.findWhere(activity_list, {name: name_to_find});
-    return new Activity(object_found.name, object_found.createdAt, object_found.register, object_found.bid, object_found.count);
-};
-
-Activity.alter_status = function (name_of_activity, type_to_alter, status_to_alter) {
-    var activity_list = Activity.get_all_items();
-    var activity_found = _(activity_list).findWhere({name: name_of_activity})
-    type_to_alter == "register"? activity_found.register = status_to_alter: activity_found.bid = status_to_alter;
-    type_to_alter == "bid" && status_to_alter == "run"? activity_found.count += 1 : false;
-    Activity.save_all(activity_list);
-    Activity.update_current_activity(activity_found);  
 };
 
 Activity.prototype.start_register = function () {
@@ -59,6 +29,40 @@ Activity.prototype.stop_bid = function () {
     Activity.alter_status(this.name, "bid", "over");
 };
 
+//内调方法（可外调）
+
+Activity.get_all_items = function () {
+    return JSON.parse(localStorage.getItem("activity_list")) || [];
+};
+
+Activity.find_in_list = function (name_of_activity) {
+    var activity_list = Activity.get_all_items();
+    return _(activity_list).findWhere({name: name_of_activity})
+};
+
+Activity.save_all = function(activity_list) {
+    localStorage.setItem("activity_list", JSON.stringify(activity_list));
+};
+
+Activity.update_current_activity = function (activity_to_update) {
+    localStorage.setItem("current_activity", JSON.stringify(activity_to_update));
+};
+
+Activity.get_current_item = function () {
+    return JSON.parse(localStorage.getItem("current_activity")) || new Activity("");
+};
+
+Activity.alter_status = function (name_of_activity, type_to_alter, status_to_alter) {
+    var activity_list = Activity.get_all_items();
+    var activity_found = _(activity_list).findWhere({name: name_of_activity})
+    type_to_alter == "register"? activity_found.register = status_to_alter: activity_found.bid = status_to_alter;
+    type_to_alter == "bid" && status_to_alter == "run"? activity_found.count += 1 : false;
+    Activity.save_all(activity_list);
+    Activity.update_current_activity(activity_found);  
+};
+
+//外调方法
+
 Activity.check_ifnot_null = function () {
     var activity_list = Activity.get_all_items();
     return !_.isEmpty(activity_list);
@@ -71,7 +75,11 @@ Activity.check_if_repeat = function (activity_name_to_check) {
 
 Activity.check_if_one_on_progress = function () {
     var activity_list = Activity.get_all_items();
-    return _(activity_list).some(function (activity_on_iterator) {
-        return activity_on_iterator.register == "run" || activity_on_iterator.bid == "run";
-    });
+    return _(activity_list).some(function (activity_on_iterator) { return activity_on_iterator.register == "run" || activity_on_iterator.bid == "run";});
 };
+
+Activity.find_by_name = function (name_to_find) {
+    var object_found = Activity.find_in_list(name_to_find);
+    return new Activity(object_found.name, object_found.createdAt, object_found.register, object_found.bid, object_found.count);
+};
+
