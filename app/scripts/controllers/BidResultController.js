26f31860e18172f9ff67db514f10c0f1cade4f0d
余泽江 <yzj1995@vip.qq.com>
2014-08-05 20:55:30 +0800

    简化BidResult控制器

--- app/scripts/controllers/BidResultController.js
+++ app/scripts/controllers/BidResultController.js
@@ -4,33 +4,29 @@ angular.module('partyBidApp')
   .controller('BidResultController', function ($scope, $location, $routeParams) {
 
 	$scope.initiate_data = function () {
-		$scope.activity_of_page = Activity.find_by_name($routeParams.name);
-		$scope.number_of_bid = parseInt($routeParams.number);
-		$scope.record_list = Bid.read_records_of_bid($scope.activity_of_page, $scope.number_of_bid);
-		$scope.count_of_records = !_.isEmpty($scope.member_list)? "(" + $scope.member_list.length.toString() + "人)": "";
-		$scope.result = Bid.compute_result($scope.activity_of_page, $scope.number_of_bid);
-		
-		if ($routeParams.new == 'true') {
-			$scope.is_modal_over = false;
+		$scope.this_activity = Activity.find_by_name($routeParams.name);
+		$scope.bid_number = parseInt($routeParams.number);
+		$scope.record_list = Bid.read_records_of_bid($scope.this_activity, $scope.bid_number);
+		$scope.count_of_records = !_.isEmpty($scope.record_list)? "(" + $scope.record_list.length.toString() + "人)": "";
+		$scope.result = Bid.compute_result($scope.this_activity, $scope.bid_number);
+		$scope.is_modal_over = $routeParams.new != 'true';
+		if (!$scope.is_modal_over) {
 			$('#myModal').modal('show');
-			window.setTimeout(function () { $scope.modal_close(); }, 3000);
+			window.setTimeout($scope.modal_close, 3000);
 		}
-		else {
-			$scope.is_modal_over = true;
-		}	
 	};
 
-	$scope.back_to_bid = function () {
-		$location.path(Url.bid_list_page($scope.activity_of_page));
+	$scope.go_to_bid = function () {
+		$location.path(Url.bid_list($scope.this_activity));
 	};
 
-	$scope.navigate_to_stats = function () {
-        $location.path(Url.bid_stats_page($scope.activity_of_page, $scope.number_of_bid));
+	$scope.go_to_stats = function () {
+        $location.path(Url.bid_stats($scope.this_activity, $scope.bid_number));
 	};
 
 	$scope.modal_close = function () {
 		$('#myModal').modal('hide');
-		$scope.$apply(function() {$scope.is_modal_over = true;});
+		$scope.$apply(function() { $scope.is_modal_over = true; });
 	};
 
 	$scope.manual_close = function () {
