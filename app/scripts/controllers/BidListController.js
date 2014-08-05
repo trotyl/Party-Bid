9aedc0b5ab478252cffa268ee00befde400ac26a
余泽江 <yzj1995@vip.qq.com>
2014-08-05 20:38:26 +0800

    简化BidList控制器

--- app/scripts/controllers/BidListController.js
+++ app/scripts/controllers/BidListController.js
@@ -4,30 +4,30 @@ angular.module('partyBidApp')
   .controller('BidListController', function ($scope, $location, $routeParams) {
 
     $scope.initiate_data = function () {
-        $scope.activity_of_page = Activity.find_by_name($routeParams.name);
-        $scope.bid_list = Bid.read_bids_of_activity($scope.activity_of_page);
-        $scope.cannot_start = Activity.on_going();
+        $scope.this_activity = Activity.find_by_name($routeParams.name);
+        $scope.bid_list = Bid.read_bids_of_activity($scope.this_activity);
+        $scope.no_start = Activity.on_going();
     };
 
     $scope.back_to_home = function () {
         $location.path(Url.home());
     };
 
-    $scope.show_bid_detail = function ($index) {
-        $location.path(Url.bid_detail($scope.activity_of_page, $scope.activity_of_page.count - $index));
+    $scope.go_to_detail = function ($index) {
+        $location.path(Url.bid_detail($scope.this_activity, $scope.this_activity.count - $index));
     };
 
-    $scope.navigate_to_register = function () {
-        $location.path(Url.activity_detail($scope.activity_of_page));
-    };
-
-    $scope.check_in_progress = function ($index) {
-        return ($scope.activity_of_page.bid == "run" && $index == 0)? "button-flat-highlight": "button-flat";
+    $scope.go_to_register = function () {
+        $location.path(Url.activity_detail($scope.this_activity));
     };
 
     $scope.start_bid = function () {
-        $scope.activity_of_page.start_bid();
-        $location.path(Url.bid_detail_page($scope.activity_of_page, $scope.activity_of_page.count + 1));
+        $scope.this_activity.start_bid();
+        $location.path(Url.bid_detail($scope.this_activity, $scope.this_activity.count + 1));
+    };
+
+    $scope.check_status = function ($index) {
+        return ($scope.this_activity.bid == "run" && $index == 0)? "button-flat-highlight": "button-flat";
     };
 
     $scope.initiate_data();
