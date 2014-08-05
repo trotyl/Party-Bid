103f94e8693bfde5ea6cb9a300981b4f93a0f2f2
余泽江 <yzj1995@vip.qq.com>
2014-08-05 16:38:16 +0800

    合并DataModel

--- app/scripts/models/DataModel.js
+++ app/scripts/models/DataModel.js
@@ -4,3 +4,12 @@ function Data () {
 Data.check_if_contains = function (list, data_to_check) {
     return _(list).findWhere(data_to_check);
 };
+
+Data.refresh_ui_list = function (page_type) {
+	var ui_scope = angular.element("#" + page_type).scope();
+	if(typeof(ui_scope.update_when_receive) == "function")  {
+		ui_scope.$apply(function () { 
+			ui_scope.update_when_receive(); 
+		});
+	}
+};
