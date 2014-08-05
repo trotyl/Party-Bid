895bdde55888198b506318a7dabb7c7027d474ad
余泽江 <yzj1995@vip.qq.com>
2014-08-05 20:33:47 +0800

    简化BidDetail控制器

--- app/scripts/controllers/BidDetailController.js
+++ app/scripts/controllers/BidDetailController.js
@@ -4,30 +4,26 @@ angular.module('partyBidApp')
   .controller('BidDetailController', function ($scope, $location, $routeParams) {
 
 	$scope.initiate_data = function () {
-		$scope.activity_of_page = Activity.find_by_name($routeParams.name);
-		$scope.number_of_bid = parseInt($routeParams.number);
-		$scope.can_not_stop = ($scope.activity_of_page.bid != "run") || ($scope.number_of_bid != $scope.activity_of_page.count);
+		$scope.this_activity = Activity.find_by_name($routeParams.name);
+		$scope.bid_number = parseInt($routeParams.number);
+		$scope.no_stop = ($scope.this_activity.bid != "run") || ($scope.bid_number != $scope.this_activity.count);
 		$scope.update_data();
 	};
 
-	$scope.back_to_bid = function () {
-		$location.path(Url.bid_list_page($scope.activity_of_page));
-	};
-
-	$scope.navigate_to_register = function () {
-		$location.path(Url.activity_detail_page($scope.activity_of_page));
+	$scope.go_to_list = function () {
+		$location.path(Url.bid_list($scope.this_activity));
 	};
 
 	$scope.end_bid = function () {
 		if(window.confirm("您确定要结束竞价么？！")) {
-        	$scope.activity_of_page.stop_bid();
-        	$location.path(Url.bid_result_page($scope.activity_of_page, $scope.number_of_bid, true));
+        	$scope.this_activity.stop_bid();
+        	$location.path(Url.bid_result($scope.this_activity, $scope.bid_number, true));
 		}
 	};
 
 	$scope.update_data = function () {
-		$scope.record_list = Bid.read_records_of_bid($scope.activity_of_page, $scope.number_of_bid);
-		$scope.count_of_records = !_.isEmpty($scope.member_list)? "(" + $scope.member_list.length.toString() + "人)": "";
+		$scope.record_list = Bid.read_records_of_bid($scope.this_activity, $scope.bid_number);
+		$scope.count_of_records = !_.isEmpty($scope.record_list)? "(" + $scope.record_list.length.toString() + "人)": "";
 	};
 
 	$scope.initiate_data();
