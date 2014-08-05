ab8027cdb31b2f9a4bd8cfee5e76d81b27b76c40
余泽江 <yzj1995@vip.qq.com>
2014-08-05 20:27:27 +0800

    简化ActivityList控制器

--- app/scripts/controllers/activityListController.js
+++ app/scripts/controllers/activityListController.js
@@ -4,22 +4,22 @@ angular.module('partyBidApp')
   .controller('ActivityListController', function ($scope, $location) {
 
     $scope.initiate_data = function () {
+        !Activity.exist() && $scope.go_to_create();
+        
         $scope.activity_list = Activity.all();
-        !Activity.exist() && $location.path(Url.create_activity());
-
-        $scope.cannot_create = Activity.on_going();
+        $scope.no_create = Activity.on_going();
     };
 
-    $scope.activity_create = function () {
+    $scope.go_to_create = function () {
     	$location.path(Url.create_activity());
     };
 
-    $scope.show_activity_detail = function (the_activity) {
-    	$location.path(Url.activity_detail_page(the_activity));
+    $scope.go_to_detail = function (the_activity) {
+    	$location.path(Url.activity_detail(the_activity));
     };
 
-    $scope.check_in_progress = function (the_activity) {
-    	return (the_activity.register == "run" || the_activity.bid == "run")? "button-flat-highlight": "button-flat";
+    $scope.check_status = function (register, bid) {
+    	return (register == "run" || bid == "run")? "button-flat-highlight": "button-flat";
     };
 
     $scope.initiate_data();
