97f94e0776ec0f689d0bc99bf3b7a66a377b5f04
余泽江 <yzj1995@vip.qq.com>
2014-08-05 16:32:15 +0800

    调整函数上下位置

--- app/scripts/controllers/BidResultController.js
+++ app/scripts/controllers/BidResultController.js
@@ -9,6 +9,7 @@ angular.module('partyBidApp')
 		$scope.record_list = Bid.read_records_of_bid($scope.activity_of_page, $scope.number_of_bid);
 		$scope.count_of_records = !_.isEmpty($scope.member_list)? "(" + $scope.member_list.length.toString() + "人)": "";
 		$scope.result = Bid.compute_result($scope.activity_of_page, $scope.number_of_bid);
+		
 		if ($routeParams.new == 'true') {
 			$scope.is_modal_over = false;
 			$('#myModal').modal('show');
@@ -19,6 +20,14 @@ angular.module('partyBidApp')
 		}	
 	};
 
+	$scope.back_to_bid = function () {
+		$location.path(Url.bid_list_page($scope.activity_of_page));
+	};
+
+	$scope.navigate_to_stats = function () {
+        $location.path(Url.bid_stats_page($scope.activity_of_page, $scope.number_of_bid));
+	};
+
 	$scope.modal_close = function () {
 		$('#myModal').modal('hide');
 		$scope.$apply(function() {$scope.is_modal_over = true;});
@@ -26,14 +35,6 @@ angular.module('partyBidApp')
 
 	$scope.manual_close = function () {
 		$scope.is_modal_over = true;
-	};
-
-	$scope.back_to_bid = function () {
-		$location.path(Url.bid_list_page($scope.activity_of_page));
-	};
-
-	$scope.navigate_to_stats = function () {
-        $location.path(Url.bid_stats_page($scope.activity_of_page, $scope.number_of_bid));
 	};
 
 	$scope.initiate_data();
