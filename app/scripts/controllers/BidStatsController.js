42237f341352201682d0b3768f210d24bb21cbbd
余泽江 <yzj1995@vip.qq.com>
2014-08-12 17:07:58 +0800

    竞价统计计数采用filter实现，返回改用Directive

--- app/scripts/controllers/BidStatsController.js
+++ app/scripts/controllers/BidStatsController.js
@@ -7,12 +7,7 @@ angular.module('partyBidApp')
 		$scope.this_activity = Activity.find_by_name($routeParams.name);
 		$scope.bid_number = parseInt($routeParams.number);
 		$scope.record_list = Bid.read_stats_of_bid($scope.this_activity, $scope.bid_number);
-		$scope.count_of_records = !_.isEmpty($scope.record_list)? "(" + $scope.record_list.length.toString() + "人)": "";
 		$scope.result = Bid.compute_result($scope.this_activity, $scope.bid_number);
-	};
-
-	$scope.go_to_list = function () {
-		$location.path(Url.bid_list($scope.this_activity));
 	};
 
 	$scope.go_to_result = function () {
