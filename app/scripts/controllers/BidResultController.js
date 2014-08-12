812f842dfa981e100c8fb2cf883da353555cd0e1
余泽江 <yzj1995@vip.qq.com>
2014-08-12 17:04:08 +0800

    竞价结果计数采用filter实现，返回该用Directive

--- app/scripts/controllers/BidResultController.js
+++ app/scripts/controllers/BidResultController.js
@@ -7,17 +7,12 @@ angular.module('partyBidApp')
 		$scope.this_activity = Activity.find_by_name($routeParams.name);
 		$scope.bid_number = parseInt($routeParams.number);
 		$scope.record_list = Bid.read_records_of_bid($scope.this_activity, $scope.bid_number);
-		$scope.count_of_records = !_.isEmpty($scope.record_list)? "(" + $scope.record_list.length.toString() + "人)": "";
 		$scope.result = Bid.compute_result($scope.this_activity, $scope.bid_number);
 		$scope.is_modal_over = $routeParams.new != 'true';
 		if (!$scope.is_modal_over) {
 			$('#myModal').modal('show');
 			window.setTimeout(function () {$scope.$apply($scope.modal_close)}, 3000);
 		}
-	};
-
-	$scope.go_to_list = function () {
-		$location.path(Url.bid_list($scope.this_activity));
 	};
 
 	$scope.go_to_stats = function () {
