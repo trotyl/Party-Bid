ffa3f7b26147109d10e3d357554d4370d9fe8671
余泽江 <yzj1995@vip.qq.com>
2014-08-21 10:08:13 +0800

    增加 创建活动 步骤定义 模板

---
+++ features/step_definitions/step_createActivity.rb
@@ -0,0 +1,15 @@
+Given(/^there is (\d+) activity$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^it's now in (\w+) page$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the (.*?) (\w) should be (\w*?)$/) do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the input activity name is (.*?)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
